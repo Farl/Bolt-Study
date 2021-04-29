444f6abbcad3485623804b3a83918790e591b5c8
Farl Lee <spring_snow@hotmail.com>
2021-04-29 19:47:20 +0800

    [Farl] Update bolt explorer for generic monobehaviour

--- Assets/Editor/BoltExplorer.cs
+++ Assets/Editor/BoltExplorer.cs
@@ -1,88 +1,147 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Reflection;
 using UnityEditor;
+using Ludiq;
+using Bolt;
 
 public class BoltExplorer : EditorWindow
 {
 
-    [MenuItem("Tool/Bolt Explorer")]
+    [MenuItem("JetGen/Bolt/Bolt Explorer")]
     static void Open()
     {
         var window = EditorWindow.CreateWindow<BoltExplorer>();
     }
 
-    static Bolt.FlowMachine targetFlowMachine;
-    static Bolt.FlowMachine[] flowMachines;
+    static MonoBehaviour targetMonoBehaviour;
+    static MonoBehaviour[] monoBehaviours;
     static string filter = string.Empty;
 
     private class UnitData
     {
         public string hierachy;
         public Scene scene;
-        public Bolt.FlowMachine flowMachine;
-        public Bolt.FlowGraph flowGraph;
+        public MonoBehaviour monoBehaviour;
+        public FlowMacro flowMacro;
+        public FlowGraph flowGraph;
+        public IUnit unit;
+
+        public List<PortData> portDatas = new List<PortData>();
+    }
+
+    private class PortData
+    {
         public bool isInput;
-        public Bolt.ValueInput valueInput;
+        public ValueInput valueInput;
         public object inputValue;
-        public Bolt.ValueOutput valueOutput;
+        public ValueOutput valueOutput;
         public object outputValue;
     }
 
     private class SearchResult
     {
         public UnitData unitData;
+        public int portIndex = -1;
     }
 
-    static List<Bolt.FlowMachine> flowMachineList = new List<Bolt.FlowMachine>();
+    static List<MonoBehaviour> monoBehaviourList = new List<MonoBehaviour>();
     static List<UnitData> unitList = new List<UnitData>();
     static List<SearchResult> searchResultList = new List<SearchResult>();
+
+    private enum Mode
+    {
+        Scene = 0,
+        Project = 1
+    }
+    static Mode mode = Mode.Scene;
 
     void Collect()
     {
         GameObject[] gos = Resources.FindObjectsOfTypeAll<GameObject>();
         foreach (var go in gos)
         {
-            var fms = go.GetComponentsInChildren<Bolt.FlowMachine>();
-            Collect(fms);
+            if ((mode == Mode.Scene) == go.scene.IsValid())
+            {
+                var fms = go.GetComponentsInChildren<MonoBehaviour>();
+                Collect(fms);
+            }
         }
     }
 
-    void Collect (Bolt.FlowMachine[] fms)
+    void Collect(MonoBehaviour[] mbs)
     {
-        foreach (var f in fms)
+        foreach (var f in mbs)
         {
             Collect(f);
         }
     }
 
-    void Collect(Bolt.FlowMachine fm)
+    void Collect(MonoBehaviour mb)
     {
-        if (fm == null)
+        if (mb == null)
             return;
 
-        var hierachy = (fm.gameObject.scene.IsValid()? fm.gameObject.scene.name: "[Prefab]") + "/" + fm.name;
-        flowMachineList.Add(fm);
+        var hierachy = (mb.gameObject.scene.IsValid() ? mb.gameObject.scene.name : "[Prefab]") + "/" + mb.name;
 
-        var graph = fm.graph;
-        Collect(graph, fm, hierachy);
+        FlowGraph graph;
+
+        // Find FlowMacro
+        var mbType = mb.GetType();
+
+        if (mbType == typeof(FlowMachine))
+        {
+            var fm = mb as FlowMachine;
+            graph = fm.graph;
+            Collect(graph, fm, fm.nest.macro, hierachy);
+        }
+        else
+        {
+            FieldInfo[] fis = mbType.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            foreach (var fi in fis)
+            {
+                var fType = fi.FieldType;
+                if (fType == typeof(FlowMacro))
+                {
+                    var flowMacro = fi.GetValue(mb) as FlowMacro;
+                    if (flowMacro != null && flowMacro.graph != null)
+                    {
+                        graph = flowMacro.graph;
+                        monoBehaviourList.Add(mb);
+                        Collect(graph, mb, flowMacro, hierachy);
+                    }
+                }
+            }
+        }
     }
 
-    void Collect(Bolt.FlowGraph graph, Bolt.FlowMachine fm, string hierachy)
+    void Collect(FlowGraph graph, MonoBehaviour mb, FlowMacro flowMacro, string hierachy)
     {
         foreach (var unit in graph.units)
         {
-            if (unit.GetType() == typeof(Bolt.SuperUnit))
+            var unitData = new UnitData()
             {
-                var superUnit = unit as Bolt.SuperUnit;
+                unit = unit,
+                flowGraph = graph,
+                flowMacro = flowMacro,
+                monoBehaviour = mb,
+                hierachy = hierachy,
+                scene = mb.gameObject.scene
+            };
+            unitList.Add(unitData);
+
+            // Traverse into SuperUnit
+            if (unit.GetType() == typeof(SuperUnit))
+            {
+                var superUnit = unit as SuperUnit;
                 if (superUnit != null && superUnit.nest.graph != graph)
-                    Collect(superUnit.nest.graph, fm, hierachy + "/" + superUnit.ToString());
+                    Collect(superUnit.nest.graph, mb, flowMacro, hierachy + "/" + superUnit.ToString());
             }
             foreach (var input in unit.inputs)
             {
-                var valueInput = input as Bolt.ValueInput;
+                var valueInput = input as ValueInput;
                 if (valueInput != null)
                 {
                     var t = valueInput.type;
@@ -91,22 +150,18 @@ public class BoltExplorer : EditorWindow
                     if (t == typeof(string))
                     {
                         // Collect!
-                        var unitData = new UnitData()
+                        var portData = new PortData()
                         {
                             isInput = true,
                             valueInput = valueInput,
-                            flowGraph = graph,
-                            flowMachine = fm,
-                            hierachy = hierachy,
-                            scene = fm.gameObject.scene
                         };
-                        unitList.Add(unitData);
+                        unitData.portDatas.Add(portData);
                     }
                 }
             }
             foreach (var output in unit.outputs)
             {
-                var valueOutput = output as Bolt.ValueOutput;
+                var valueOutput = output as ValueOutput;
                 if (valueOutput != null)
                 {
                     var t = valueOutput.type;
@@ -115,16 +170,12 @@ public class BoltExplorer : EditorWindow
                     if (t == typeof(string))
                     {
                         // Collect!
-                        var unitData = new UnitData()
+                        var portData = new PortData()
                         {
                             isInput = false,
                             valueOutput = valueOutput,
-                            flowGraph = graph,
-                            flowMachine = fm,
-                            hierachy = hierachy,
-                            scene = fm.gameObject.scene
                         };
-                        unitList.Add(unitData);
+                        unitData.portDatas.Add(portData);
                     }
                 }
             }
@@ -138,78 +189,36 @@ public class BoltExplorer : EditorWindow
 
         scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
         {
-            targetFlowMachine = (Bolt.FlowMachine)EditorGUILayout.ObjectField(targetFlowMachine, typeof(Bolt.FlowMachine), true);
+            targetMonoBehaviour = (FlowMachine)EditorGUILayout.ObjectField(targetMonoBehaviour, typeof(FlowMachine), true);
             EditorGUILayout.LabelField("Unit Count = " + unitList.Count);
-            EditorGUILayout.LabelField("FlowMachine Count = " + flowMachineList.Count);
+            EditorGUILayout.LabelField("MonoBehaviour Count = " + monoBehaviourList.Count);
+
+            mode = (Mode)EditorGUILayout.EnumPopup(mode);
 
             if (GUILayout.Button("Collect"))
             {
                 unitList.Clear();
-                flowMachineList.Clear();
-                if (targetFlowMachine)
+                monoBehaviourList.Clear();
+                if (targetMonoBehaviour)
                 {
-                    Collect(targetFlowMachine);
+                    Collect(targetMonoBehaviour);
                 }
                 else
                 {
                     Collect();
                 }
             }
+
             filter = EditorGUILayout.TextField("Filter", filter);
+
             if (GUILayout.Button("Search"))
             {
                 searchResultList.Clear();
                 foreach (UnitData ud in unitList)
                 {
-                    var t = ud.isInput? ud.valueInput.GetType(): ud.valueOutput.GetType();
-                    var isFound = false;
-                    if (ud.isInput)
-                    {
-                        // Get Value input default value
-                        PropertyInfo defaultValuePI = t.GetProperty("_defaultValue", BindingFlags.NonPublic | BindingFlags.Instance);
-                        if (ud.valueInput.hasDefaultValue)
-                        {
-                            if (defaultValuePI != null)
-                            {
-                                try
-                                {
-                                    var value = defaultValuePI.GetValue(ud.valueInput);
-                                    ud.inputValue = value;
-                                    if (string.IsNullOrEmpty(filter) || value.ToString().IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0)
-                                    {
-                                        isFound = true;
-                                    }
-                                }
-                                catch (System.Exception e)
-                                {
-                                    Debug.LogException(e, ud.flowMachine);
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        // Get Value output value (function)
-                        FieldInfo getValueFI = t.GetField("getValue", BindingFlags.NonPublic | BindingFlags.Instance);
-                        if (getValueFI != null)
-                        {
-                            var getValueFunc = (System.Func<Bolt.Flow, System.Object>)getValueFI.GetValue(ud.valueOutput);
-                            var obj = getValueFunc.Target;
-                            if (obj.GetType() == typeof(Bolt.Literal))
-                            {
-                                var literal = obj as Bolt.Literal;
-                                var value = literal.value;
-                                ud.outputValue = value;
-                                if (string.IsNullOrEmpty(filter) || value.ToString().IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0)
-                                {
-                                    isFound = true;
-                                }
-                            }
-                        }
-                    }
 
-                    // Found!
-                    if (isFound)
+                    // Match unit name
+                    if (ud.unit.ToString().IndexOf(filter.Replace(" ", string.Empty), System.StringComparison.OrdinalIgnoreCase) >= 0)
                     {
                         var sr = new SearchResult()
                         {
@@ -217,30 +226,99 @@ public class BoltExplorer : EditorWindow
                         };
                         searchResultList.Add(sr);
                     }
+
+                    for (int i = 0; i < ud.portDatas.Count; i++)
+                    {
+                        var pd = ud.portDatas[i];
+
+                        var t = pd.isInput ? pd.valueInput.GetType() : pd.valueOutput.GetType();
+
+                        if (pd.isInput)
+                        {
+                            // Get Value input default value
+                            PropertyInfo defaultValuePI = t.GetProperty("_defaultValue", BindingFlags.NonPublic | BindingFlags.Instance);
+                            if (pd.valueInput.hasDefaultValue)
+                            {
+                                if (defaultValuePI != null)
+                                {
+                                    try
+                                    {
+                                        var value = defaultValuePI.GetValue(pd.valueInput);
+                                        pd.inputValue = value;
+                                        if (string.IsNullOrEmpty(filter) || (value != null && value.ToString().IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0))
+                                        {
+                                            var sr = new SearchResult()
+                                            {
+                                                unitData = ud,
+                                                portIndex = i
+                                            };
+                                            searchResultList.Add(sr);
+                                        }
+                                    }
+                                    catch (System.Exception e)
+                                    {
+                                        Debug.LogException(e, ud.monoBehaviour);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // Get Value output value (function)
+                            FieldInfo getValueFI = t.GetField("getValue", BindingFlags.NonPublic | BindingFlags.Instance);
+                            if (getValueFI != null)
+                            {
+                                var getValueFunc = (System.Func<Flow, System.Object>)getValueFI.GetValue(pd.valueOutput);
+                                var obj = getValueFunc.Target;
+                                if (obj.GetType() == typeof(Literal))
+                                {
+                                    var literal = obj as Literal;
+                                    var value = literal.value;
+                                    pd.outputValue = value;
+                                    if (string.IsNullOrEmpty(filter) || value.ToString().IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0)
+                                    {
+                                        var sr = new SearchResult()
+                                        {
+                                            unitData = ud,
+                                            portIndex = i
+                                        };
+                                        searchResultList.Add(sr);
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
             foreach (var sr in searchResultList)
             {
                 EditorGUILayout.BeginHorizontal();
-                var fm = sr.unitData.flowMachine;
-                var unit = sr.unitData.isInput? sr.unitData.valueInput.unit: sr.unitData.valueOutput.unit;
+                var fm = sr.unitData.monoBehaviour;
+                var unit = sr.unitData.unit;
                 var graph = sr.unitData.flowGraph;
+                var flowMacro = sr.unitData.flowMacro;
+
                 EditorGUILayout.LabelField(sr.unitData.hierachy);
-                EditorGUILayout.ObjectField(fm, typeof(Bolt.FlowMachine), true);
+                EditorGUILayout.ObjectField(fm, typeof(FlowMachine), true);
                 if (GUILayout.Button("Focus"))
                 {
-                    var gw = EditorWindow.GetWindow<Ludiq.GraphWindow>();
+                    //var gw = EditorWindow.GetWindow<Ludiq.GraphWindow>();
                     Selection.activeObject = fm;
+                    GraphWindow.OpenActive(GraphReference.New(flowMacro, true));
                     graph.pan = unit.position;
                 }
-                var portName = sr.unitData.isInput ? "> " + sr.unitData.valueInput.key : "< " + sr.unitData.valueOutput.key;
-                var valueContent = sr.unitData.isInput ? sr.unitData.inputValue as string : sr.unitData.outputValue as string;
-                EditorGUILayout.LabelField(string.Format("{0} = {1}", portName, valueContent));
+
+                var pd = sr.portIndex >= 0 ? sr.unitData.portDatas[sr.portIndex] : null;
+                if (pd != null)
+                {
+                    var portName = pd.isInput ? "> " + pd.valueInput.key : "< " + pd.valueOutput.key;
+                    var valueContent = pd.isInput ? pd.inputValue as string : pd.outputValue as string;
+                    EditorGUILayout.LabelField(string.Format("{0} = {1}", portName, valueContent));
+                }
                 EditorGUILayout.EndHorizontal();
             }
         }
         EditorGUILayout.EndScrollView();
-
     }
 }
